6b48abc69aeca9839a8c7a534f7843bc5fd101ac
JohnSamuelOlamide <samueljohnolamide@gmail.com>
2025-05-28 06:14:15 +0100

    Responsive View

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,6 +44,43 @@ const scrollUp = () => {
     : scrollUp.classList.remove("show-scroll");
 };
 window.addEventListener("scroll", scrollUp);
+
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
+
+const scrollActive = () => {
+  const scrollDown = window.scrollY;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionsClass = document.querySelector(
+        ".nav__list a[href*=" + sectionId + "]"
+      );
+
+    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add("active-link");
+    } else {
+      sectionsClass.classList.remove("active-link");
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive);
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+  reset: true, //Repeat Reveal Animation
+});
+
+sr.reveal(`.perfil, .contact`);
+sr.reveal(`.info`, { origin: "left", delay: 800 });
+sr.reveal(`.skills`, { origin: "left", delay: 1000 });
+sr.reveal(`.about`, { origin: "right", delay: 1200 });
+sr.reveal(`.projects__card, .services__card, .experience__card`, {
+  interval: 100,
+});
